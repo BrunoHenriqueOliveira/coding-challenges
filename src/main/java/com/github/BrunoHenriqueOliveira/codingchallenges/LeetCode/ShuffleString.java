f57fddf3478692d1c11eb0bce01b2394824b616e
BrunoHenriqueOliveira <bruno.oliveira.tc@outlook.com>
2025-02-05 22:58:39 -0300

    add: ShuffleString.java

---
+++ src/main/java/com/github/BrunoHenriqueOliveira/codingchallenges/LeetCode/ShuffleString.java
@@ -0,0 +1,11 @@
+package com.github.BrunoHenriqueOliveira.codingchallenges.LeetCode;
+
+public class ShuffleString {
+    public String restoreString(String s, int[] indices){
+        char[] shuffled = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            shuffled[indices[i]] = s.charAt(i);
+        }
+        return new String(shuffled);
+    }
+}
